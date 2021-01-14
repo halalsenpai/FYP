54315b61c85c40ab85a298160d6840dc3134ddf0
halalsenpai <abdchaghtai@gmail.com>
2021-01-15 00:54:49 +0500

    updated hours

--- placex/src/components/StepForm/BuisnessHours.js
+++ placex/src/components/StepForm/BuisnessHours.js
@@ -1,48 +1,57 @@
 import React, { useState } from "react";
 import { Button, Checkbox, Container, Header } from "semantic-ui-react";
 import "./formstyles.css";
+
 import Hours from "./Hours";
-import HoursTest from "./Hours.test";
 
 export default function BuisnessHours({ formData, setForm, navigation }) {
-	const [isToggle, setToggle] = useState(true);
-	function toggled() {
-		setToggle(!isToggle);
-	}
+  const style = {
+    display: "flex",
+    justifyContent: "space-between",
+  };
 
-	return (
-		<>
-			<Container style={{ marginTop: "5em" }}>
-				<h1 style={{ textAlign: "center" }}>Buisness Hours</h1>
-				<Container style={{ width: "400px" }}>
-					<Container
-						style={{ display: "flex", justifyContent: "space-between" }}
-					>
-						<Header as="h3">Always Open</Header>
-						<Checkbox
-							toggle
-							defaultChecked={isToggle}
-							onChange={toggled}
-							checked={isToggle}
-						/>
-					</Container>
-					<Container
-						style={{ display: "flex", justifyContent: "space-between" }}
-					>
-						<Header as="h3">Open Specific Hours</Header>
-						<Checkbox toggle checked={!isToggle} onChange={toggled} />
-					</Container>
-				</Container>
-				{!isToggle ? <HoursTest /> : null}
-				<Button.Group fluid>
-					<Button secondary onClick={navigation.previous}>
-						Back
-					</Button>
-					<Button positive onClick={navigation.next}>
-						Next
-					</Button>
-				</Button.Group>
-			</Container>
-		</>
-	);
+  const [isToggle, setToggle] = useState(true);
+  function toggled() {
+    setToggle(!isToggle);
+  }
+
+  return (
+    <>
+      <Container style={{ marginTop: "5em" }}>
+        <h1 style={{ textAlign: "center" }}>Buisness Hours</h1>
+        <div style={{ width: "500px", margin: "0 auto" }}>
+          <div style={style}>
+            <Header as="h3">Always Open</Header>
+            <Checkbox
+              toggle
+              defaultChecked={isToggle}
+              onChange={toggled}
+              checked={isToggle}
+            />
+          </div>
+          <div style={style}>
+            <Header as="h3">Open Specific Hours</Header>
+            <Checkbox toggle checked={!isToggle} onChange={toggled} />
+          </div>
+        </div>
+        {!isToggle ? <Hours /> : null}
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "500px",
+            margin: "10px auto",
+          }}
+        >
+          <Button secondary onClick={navigation.previous}>
+            Back
+          </Button>
+          <Button positive onClick={navigation.next}>
+            Next
+          </Button>
+        </div>
+      </Container>
+    </>
+  );
 }
--- placex/src/components/StepForm/Hours.js
+++ placex/src/components/StepForm/Hours.js
@@ -1,86 +1,28 @@
-import React, { useState } from "react";
-import { Checkbox } from "semantic-ui-react";
-import "./formstyles.css";
-import Time from "./Time";
+import React, { useState } from 'react'
+import Hourdiv from './Hourdiv';
 
-export default function Hours() {
-	const [isMonday, setIsMonday] = useState(false);
+export default function HoursTest(props) {
+    const [isMonday, setIsMonday] = useState(false);
 	const [isTuesday, setisTuesday] = useState(false);
 	const [isWednesday, setisWednesday] = useState(false);
 	const [isThursday, setisThursday] = useState(false);
 	const [isFriday, setisFriday] = useState(false);
 	const [isSaturday, setisSaturday] = useState(false);
-	const [isSunday, setisSunday] = useState(false);
-	
-	function toggle(x,y) {
-		// setIsMonday(!isMonday);
-		x(!y)
-		
-			
-	
-	}
+    const [isSunday, setisSunday] = useState(false);
+    
+    const days = [
+        {name:'Monday', setDay:setIsMonday, day:isMonday , id:1},
+        {name:'Tuesday', setDay:setisTuesday, day:isTuesday , id:2},
+        {name:'Wednesday', setDay:setisWednesday, day:isWednesday , id:3},
+        {name:'Thursday', setDay:setisThursday, day:isThursday , id:4},
+        {name:'Friday', setDay:setisFriday, day:isFriday , id:5},
+        {name:'Saturday', setDay:setisSaturday, day:isSaturday , id:6},
+        {name:'Sunday', setDay:setisSunday, day:isSunday , id:7}
+    ]
 
-	
-
-	return (
-		<div>
-			<div className="hour_div">
-				
-					<div className='toggle-div' style={{width:'50%' , display:'flex' , justifyContent:'space-between'}}>
-						<span>Monday</span>
-						<Checkbox toggle onChange={()=>toggle(setIsMonday,isMonday)} />
-					</div>
-				
-				{isMonday ? <Time /> : null}
-			</div>
-			<div className="hour_div">
-				<div className='toggle-div'>
-					<span>Tuesday</span>
-					<Checkbox toggle onChange={()=>toggle(setisTuesday,isTuesday)} />
-				</div>
-
-				{isTuesday ? <Time /> : null}
-			</div>
-			<div className="hour_div">
-			<div className='toggle-div'>
-					<span>Wednesday</span>
-					<Checkbox toggle onChange={()=>toggle(setisWednesday,isWednesday)} />
-			</div>
-
-				{isWednesday ? <Time /> : null}
-			</div>
-			<div className="hour_div">
-		<div className='toggle-div'>
-					<span>Thursday</span>
-					<Checkbox toggle onChange={()=>toggle(setisThursday,isThursday)} />
-		</div>
-
-				{isThursday ? <Time /> : null}
-			</div>
-			<div className="hour_div">
-			<div className='toggle-div'>
-					<span>Friday</span>
-					<Checkbox toggle onChange={()=>toggle(setisFriday,isFriday)} />
-			</div>
-
-				{isFriday ? <Time /> : null}
-			</div>
-			<div className="hour_div">
-				<div className='toggle-div'>
-					<span>Saturday</span>
-					<Checkbox toggle onChange={()=>toggle(setisSaturday,isSaturday)} />
-				</div>
-
-				{isSaturday ? <Time /> : null}
-			</div>
-			<div className="hour_div">
-				<div className='toggle-div'>
-					<span>Sunday</span>
-					<Checkbox toggle onChange={()=>toggle(setisSunday,isSunday)} />
-				</div>
-
-				{isSunday ? <Time /> : null}
-			</div>
-		</div>
-	);
+    return (
+        <div>
+           {days.map((day)=>(<Hourdiv days={day} key={days.id}/>))} 
+        </div>
+    )
 }
